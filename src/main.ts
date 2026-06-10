4a0a468b08e45adb3ffc2430e87e317aca08b3a1
Shane B. <dotfortun@users.noreply.github.com>
2026-06-10 15:00:21 +0000

    Talked functions and iteration.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,48 @@
-if (typeof document !== "undefined") {
-  import("./style.css").then(() => {
-    const app = document.querySelector<HTMLParagraphElement>("#app");
-    if (app) {
-      app.textContent = "If you can see this, Tailwind is working.";
+window.onload = () => {
+  import("./style.css");
+
+  const sombra = {
+    name: "Sombra",
+    nickname: "Little Sombrita",
+    yellForFood: () => {
+      console.log(
+        "I'M HUNGRY, I HAVE NEVER SEEN FOOD IN MY LIFE.  WHAT DO YOU MEAN IT IS 4AM?",
+      );
+    },
+  };
+
+  // function doSomething(foo: string, bar: string) {
+  //   console.log("This function has been called!");
+  //   console.log("foo:", foo);
+  //   console.log("bar:", bar);
+  // }
+
+  // doSomething("Sombra", "Grizelle");
+
+  // const helloWorld = (name: string) => {
+  //   console.log(`Hello, ${name}`);
+  // };
+
+  // const inlineHello = (name: string) => console.log(`Hello, ${name}`);
+
+  // helloWorld("Sombra");
+  // inlineHello("Grizelle");
+
+  const fib = (n: number = 0): number => {
+    let a = 1;
+    let b = 0;
+    let swap = 0;
+
+    for (let iter = 0; iter < n; iter++) {
+      swap = a + b;
+      b = a;
+      a = swap;
     }
-  });
-}
 
-console.log("Hello from src/main.ts");
+    return a;
+  };
 
-export {};
+  for (let i = 0; i < 10; i++) {
+    console.log(`fib(${i}):`, fib(i));
+  }
+};
